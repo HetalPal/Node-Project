d6b22734f9600f05b471a0cbde6e5d7c342a4b2f
HetalPal <htlpal01@gmail.com>
2026-02-26 13:13:07 -0400

    PR

--- PR-6/config/dbConnect.js
+++ PR-6/config/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnect = () =>{
-    mongoose.connect("mongodb+srv://htlpal01_db_user:hetal0102@cluster0.4wsx3kh.mongodb.net/Admin-panel")
+    mongoose.connect('mongodb+srv://htlpal01_db_user:hetal0102@cluster0.4wsx3kh.mongodb.net/PR-6')
     .then(() => console.log('DB Connected!!!'))
     .catch(err => console.log(err));
 }
